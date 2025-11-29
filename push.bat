4aa22312f0f8ed4753c1bf6002e1b72cfe161928
rongzy <rozy@qq.com>
2025-11-29 13:41:29 +0800

    2025-11-29 周六

--- push.bat
+++ push.bat
@@ -1,4 +1,14 @@
+@echo off
+setlocal
+
+:: 获取当前日期（格式：YYYY-MM-DD）
+for /f "tokens=1-3 delims=-/" %%a in ("%date%") do (
+    set y=%%a
+    set m=%%b
+    set d=%%c
+)
+
 git add .
-git commit -m "update"
+git commit -m "%y%-%m%-%d%"
 git push
 
